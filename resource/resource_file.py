098bc12b0f265dcb0a0507032d13f9ce53f4c424
Jimmy Jen <jjen@rocketsoftware.com>
2019-10-12 22:04:08 -0400

    updated xml to text mimetype

--- resource/resource_file.py
+++ resource/resource_file.py
@@ -5,7 +5,7 @@ Passing Values
 file_jpeg = {'file_name': './files/hqdefault.jpg', 'mimetype': 'image/jpeg'}
 file_png = {'file_name': './files/drop_down_ID.png', 'mimetype': 'image/png'}
 file_json = {'file_name': './files/tsconfig.json', 'mimetype': 'application/json'}
-file_xml = {'file_name': './files/fetch.xml', 'mimetype': 'application/xml'}
+file_xml = {'file_name': './files/fetch.xml', 'mimetype': 'text/xml'}
 valid_url = '1yoefGB05bkj14jBGupEhfegZ80kM2QKy'
 
 file_list = [file_jpeg, file_json, file_png, file_xml]
